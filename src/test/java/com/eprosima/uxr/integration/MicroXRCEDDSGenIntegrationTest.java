d11efd62960c1f75eb8fc7a51fca4ba3f8f0b8f7
Ricardo González Moreno <ricardo@richiware.dev>
2023-03-01 11:33:19 +0100

    Refs #17409. Update to gradle 7 and last idl-parser
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- src/test/java/com/eprosima/uxr/integration/MicroXRCEDDSGenIntegrationTest.java
+++ src/test/java/com/eprosima/uxr/integration/MicroXRCEDDSGenIntegrationTest.java
@@ -1,13 +1,13 @@
 package com.eprosima.uxr.integration;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.eprosima.integration.Command;
 import com.eprosima.integration.IDL;
 import com.eprosima.integration.TestManager;
 import com.eprosima.integration.TestManager.TestLevel;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 
@@ -42,7 +42,7 @@ public class MicroXRCEDDSGenIntegrationTest
         ArrayList<String[]> commands = new ArrayList<String[]>();
         commands.add(new String[]{"mkdir -p " + OUTPUT_PATH, "."});
         commands.add(new String[]{"rm -rf Micro-XRCE-DDS-Client", OUTPUT_PATH});
-        commands.add(new String[]{"git clone -b " + branch + " https://github.com/eProsima/Micro-XRCE-DDS-Client.git", OUTPUT_PATH});
+        commands.add(new String[]{"git clone -q -c advice.detachedHead=false -b " + branch + " https://github.com/eProsima/Micro-XRCE-DDS-Client.git", OUTPUT_PATH});
         commands.add(new String[]{"mkdir build", OUTPUT_PATH + "/Micro-XRCE-DDS-Client"});
         commands.add(new String[]{"cmake .. -DCMAKE_INSTALL_PREFIX=install", OUTPUT_PATH + "/Micro-XRCE-DDS-Client/build"});
         commands.add(new String[]{"make", OUTPUT_PATH + "/Micro-XRCE-DDS-Client/build"});
@@ -50,7 +50,7 @@ public class MicroXRCEDDSGenIntegrationTest
 
         for(String[] command: commands)
         {
-            if(!Command.execute(command[0], command[1], true))
+            if(!Command.execute(command[0], command[1], true, false))
             {
                 System.exit(-1);
             }
@@ -60,20 +60,17 @@ public class MicroXRCEDDSGenIntegrationTest
         TestManager tests = new TestManager(TestLevel.RUN, "share/microxrcedds/microxrceddsgen", INPUT_PATH, OUTPUT_PATH + "/idls");
         tests.addCMakeArguments("-DCMAKE_PREFIX_PATH=" + System.getProperty("user.dir") + "/" + OUTPUT_PATH + "/Micro-XRCE-DDS-Client/build/install");
         tests.removeTests(
-            IDL.ARRAY_NESTED, 
-            IDL.SEQUENCE_NESTED,
-            IDL.SEQUENCE_BASIC,
-            IDL.SEQUENCE_STRING,
-            IDL.SEQUENCE_CUSTOM,
-            IDL.INCLUDE,
-            IDL.BITSET_BITMASK,
-            IDL.COVERAGE_BASIC,
-            IDL.COVERAGE_COMPLEX,
-            IDL.FORWARD_DECLS,
-            IDL.NEW_FEATURES_4_2
-        );
-
-        // Remove tests here
+                IDL.ARRAY_NESTED,
+                IDL.BITSET_BITMASK,
+                IDL.COVERAGE_BASIC,
+                IDL.COVERAGE_COMPLEX,
+                IDL.FORWARD_DECLS,
+                IDL.NEW_FEATURES_4_2,
+                IDL.SEQUENCE_BASIC_IN_MODULE_TYPEDEF,
+                IDL.SEQUENCE_CUSTOM_IN_MODULE_TYPEDEF,
+                IDL.SEQUENCE_STRING_IN_MODULE_TYPEDEF,
+                IDL.SEQUENCE_NESTED
+                );
         boolean testResult = tests.runTests();
         System.exit(testResult ? 0 : -1);
     }
