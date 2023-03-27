8da3152ef54a0a3863756078c763738575cda9b8
acuadros95 <acuadros1995@gmail.com>
2023-03-27 08:23:11 +0200

    Add argument to help output
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>

--- src/main/java/com/eprosima/uxr/microxrceddsgen.java
+++ src/main/java/com/eprosima/uxr/microxrceddsgen.java
@@ -237,6 +237,7 @@ public class microxrceddsgen {
         System.out.println("\t\t-replace: replaces existing generated files.");
         System.out.println("\t\t-ppDisable: disables the preprocessor.");
         System.out.println("\t\t-ppPath: specifies the preprocessor path.");
+        System.out.println("\t\t-I <path>: add directory to preprocessor include paths.");
         System.out.println("\t\t-d <path>: sets an output directory for generated files.");
         System.out.println("\t\t-t <temp dir>: sets a specific directory as a temporary directory.");
         System.out.println("\tand the supported input files are:");
